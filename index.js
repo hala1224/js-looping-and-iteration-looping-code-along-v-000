e3eb6c5e4cd153c42e3659e4d9fc3d764053cffb
Natalie Halawani <hala1224@gmail.com>
2018-10-15 04:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ function printBadges(names) {
   }
   return names;
 }
-  let count=0;
+//   let count=0;
+// 	function tailsNeverFails() {
+// 	 // let count=0;
+// 	  let tails= Math.random();
+// 	  if (tails >= 0.5) {
+//       count+=1;
+//       tailsNeverFails();
+// 	  } else {
+// 	    console.log(`You got ${count} tails in a row!`);
+// 	  }
+// 	}
+	let count=0;
 	function tailsNeverFails() {
-	 // let count=0;
-	  let tails= Math.random();
-	  if (tails >= 0.5) {
-       count+=1;
-       tailsNeverFails();
-	  } else {
-	    console.log(`You got ${count} tails in a row!`);
-	  }
-	}
+
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+ 
+  while (maybeTrue()) {
+    count+=1;
+  }
+  console.log(`You got ${count} tails in a row!`);
+  }
