ad98c50805f6e7284d9b95146d3fe1878844b816
Natalie Halawani <hala1224@gmail.com>
2018-10-15 03:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function printBadges(names) {
 }
 
 	function tailsNeverFails() {
-	  
+	  return Math.random() >= 0.5;
+}
+ 
+while (maybeTrue()) {
+    console.log('And I ran... I ran so far away!');
 	}
